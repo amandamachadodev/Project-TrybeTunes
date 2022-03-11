acf51588d2f414018a32f3bfc69355c22d336221
Your Name <you@example.com>
2022-03-11 01:12:46 -0300

    Requisito 5 em adamento

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,11 +2,45 @@ import React from 'react';
 import Header from '../component/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisabled: true,
+    };
+  }
+
+  buttonAble = (event) => {
+    const artistInput = event.target.value;
+    const thwo = 2;
+    if (artistInput.length >= thwo) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="artist">
+            <input
+              type="text"
+              data-testid="link-to-profile"
+              onChange={ this.buttonAble }
+              placeholder="Nome do artista"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
